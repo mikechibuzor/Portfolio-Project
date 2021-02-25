c105dde12fdf9ac4f26053dc51262a1f1dd77082
__Chi__ <iwuagwuchibuzor@gmail.com>
2021-02-25 18:33:02 +0100

    changed the font-family, font-size, product-category background and edits like this

--- src/App.vue
+++ src/App.vue
@@ -21,12 +21,12 @@ export default {
 
 <style>
 #app {
-  font-family:  Georgia, "Times New Roman", Times, serif;
-  -webkit-font-smoothing: antialiased;
+  /* font-family:  Georgia, "Times New Roman", Times, serif; */
+  font-family: 'Quicksand', sans-serif;
+ -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   overflow-x: hidden;
-
 }
 
 /* Styling below are for the NavBar and Foot components */
--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,24 +1,25 @@
 <template>
-  <p>Home</p>
+
   <div class="first-content">
     <custom-carousel :imagesObject='imagesObject'></custom-carousel>
     
     <!-- Who we are starts -->
-    <div class="who-we-are flex flex-col items-center justify-center  w-screen px-6 bg-pure-blue  xl:px-32">
-        <h2 class="text-3xl mt-4 mb-2 text-white">Who We Are</h2>
-        <p class="text-center text-white font-thin">Lorem ipsum, dolor sit amet consectetur adipisicing elit. At inventore eveniet voluptas quas soluta <br />
-            adipisci est ipsa adipisci est ipsa adipisci est ipsa reiciendis eaque fugit? <br />
-            adipisci est ipsaadipisci est ipsaadipisci est ipsa
+    <div class="who-we-are h-auto xl:h-height40 flex flex-col items-center justify-center  w-screen px-6  xl:px-32">
+        <h2 class=" text-3xl xl:text-4xl mt-6 mb-2  text-white">About Us</h2>
+        <p class="text-center leading-8 mt-4 text-white text-lg  xl:px-20 ">
+          Smooches is an online store that stocks on a variety of female lingerie, men underwear, sleepwear,
+          loungewear, swimwear , sex toys and scents at best prices.<br /> The goal at SMOOCHES is to style 
+          everyone with the underwear that best flatters them.
         </p>
-        <p class="abtBtn text-center px-10 cursor-pointer hover:bg-black hover:text-white py-1  mt-6 bg-white text-red-900  mb-8 xl:mb-0">About Us</p>
+        <p class="abtBtn text-center px-10 cursor-pointer transition-all duration-200 hover:bg-black hover:text-white font-bold py-1  mt-6 bg-white text-red-600  mb-8 xl:mb-0">Read More</p>
     </div>
     <!-- Who we are ends -->
   </div>
   
     <!-- Category Starts -->
-  <div class="category w-screen bg-red-100 xl:px-12">
+  <div class="category w-screen bg-white xl:px-12">
       <div class="head flex justify-between items-center xl:items-stretch px-6 xl:px-16 py-8">
-          <h2 class="text-xl  xl:text-2xl">Shop by category</h2>
+          <h2 class="text-xl font-bold xl:text-3xl">Shop by category</h2>
           <h4 class="text-sm xl:text-base ">SHOP ALL PRODUCTS</h4>
       </div>
       <div class="products border-1 px-8 pb-10 grid grid-cols-1  xl:grid-cols-3 gap-y-10 gap-x-20  ">
@@ -87,10 +88,6 @@ export default {
 </script>
 
 <style scoped>
-
-  .who-we-are {
-  height: 35vh;
-}
 
 h1,
 h2 {
