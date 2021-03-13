d019f2abedf29b4f9b828b4e174e481574e83691
__Chi__ <iwuagwuchibuzor@gmail.com>
2021-03-13 01:13:51 +0100

    tested and corrected responsivness on mobiles

--- src/components/layout/MyLogo.vue
+++ src/components/layout/MyLogo.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="container order-2 xl:order-1 px-3 xl:px-8">
-      <div class="logo  h-12 w-12">
+    <div class="container order-2 xl:order-1  flex justify-center  px-3 xl:px-8">
+      <div class="logo   h-12 w-12">
       
-        <svg class="h-full w-full xl:mt-6" viewBox="0 0 85 85" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg class="h-full w-full  justify-center xl:mt-6" viewBox="0 0 85 85" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g filter="url(#filter0_d)">
             <circle cx="40.5" cy="38.5" r="38.5" fill="white"/>
             <circle cx="40.5" cy="38.5" r="38" stroke="#DADADA"/>
