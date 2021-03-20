429c51d3899f05f97eb263cf0fb29f5d3102b994
__Chi__ <iwuagwuchibuzor@gmail.com>
2021-03-20 18:37:54 +0100

    worked on filter upto a point

--- src/components/utilities/FilterProject.vue
+++ src/components/utilities/FilterProject.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="mt-8 flex flex-wrap ">
-      <span @click="setFilter" class="bg-text-color dark:bg-green-500 shadow-sm    text-white rounded-xl px-3 xl:px-6 py-0.5 mr-4 mb-4 cursor-pointer" id="all">All</span>
-      <span @click="setFilter" class="bg-white shadow-sm dark:text-gray-700  text-black rounded-xl px-3 xl:px-6 py-0.5 mr-4 mb-4 cursor-pointer" id="react">React</span>
-      <span @click="setFilter" class="bg-white shadow-sm dark:text-gray-700   text-black  rounded-xl px-3 xl:px-6 py-0.5 mr-4 mb-4 cursor-pointer" id="wordpress">WordPress</span>
-      <span @click="setFilter" class="bg-white shadow-sm dark:text-gray-700    text-black  rounded-xl px-3 xl:px-6 py-0.5 mr-4 mb-4 cursor-pointer" id="php">PHP</span>
+  <div ref="parent" class="mt-8 flex flex-wrap ">
+      <span @click="setFilter" :class="activeAll" class="active bg-text-color   text-white rounded-xl px-3 xl:px-6 py-0.5 mr-4 mb-4 cursor-pointer" id="all">All</span>
+      <span @click="setFilter" class="inactive bg-white shadow-sm dark:text-gray-700  text-black rounded-xl px-3 xl:px-6 py-0.5 mr-4 mb-4 cursor-pointer" id="react">React</span>
+      <span @click="setFilter" class="inactive bg-white shadow-sm dark:text-gray-700   text-black  rounded-xl px-3 xl:px-6 py-0.5 mr-4 mb-4 cursor-pointer" id="wordpress">WordPress</span>
+      <span @click="setFilter" class="inactive bg-white shadow-sm dark:text-gray-700    text-black  rounded-xl px-3 xl:px-6 py-0.5 mr-4 mb-4 cursor-pointer" id="php">PHP</span>
   </div>
 </template>
 
@@ -16,30 +16,42 @@ export default {
         all: true,
         react: false,
         wordpress: false,
-        php:false
+        php: false
       }
     }
   },
   methods: {
+    toggleActive(button){
+      button.classList.toggle('active');
+      button.classList.toggle('inactive');
+    },
     setFilter(event){
       const buttonClicked = event.target.id;
-      console.log(this.all)
-      const isActive = this[buttonClicked];
-      console.log(isActive);
-      this[buttonClicked] = !isActive;
       const updatedFilters = {
-        ...this.filters,
-        [buttonClicked]: isActive
+        all: false,
+        react: false,
+        wordpress: false,
+        php: false,
+        [buttonClicked]: !this[buttonClicked],
       }
-      this.filters = updatedFilters;
-      console.log(buttonClicked);
-      this.$emit('change-filter', updatedFilters)
-      
+      this.$emit('change-filter', updatedFilters);
+  
+  }
+},
+  computed:{
+    activeAll(){
+      return { active: this.filters.all}
     }
   }
 }
 </script>
 
 <style scoped>
+  .active{
+    background-color: blue;
+  }
 
+  /* html.dark .active{
+    background-color: green;
+  } */
 </style>
