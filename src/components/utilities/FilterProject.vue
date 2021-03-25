cfe11fca57fe8217b84f343df987bc1e8a19607f
__Chi__ <iwuagwuchibuzor@gmail.com>
2021-03-25 07:19:11 +0100

    change filter-button bg-color on clicked

--- src/components/utilities/FilterProject.vue
+++ src/components/utilities/FilterProject.vue
@@ -1,9 +1,9 @@
 <template>
   <div ref="parent" class="mt-8 flex flex-wrap ">
-      <span @click="setFilter" :class="activeAll" class="active bg-text-color   text-white rounded-xl px-3 xl:px-6 py-0.5 mr-4 mb-4 cursor-pointer" id="all">All</span>
-      <span @click="setFilter" class="inactive bg-white shadow-sm dark:text-gray-700  text-black rounded-xl px-3 xl:px-6 py-0.5 mr-4 mb-4 cursor-pointer" id="react">React</span>
-      <span @click="setFilter" class="inactive bg-white shadow-sm dark:text-gray-700   text-black  rounded-xl px-3 xl:px-6 py-0.5 mr-4 mb-4 cursor-pointer" id="wordpress">WordPress</span>
-      <span @click="setFilter" class="inactive bg-white shadow-sm dark:text-gray-700    text-black  rounded-xl px-3 xl:px-6 py-0.5 mr-4 mb-4 cursor-pointer" id="php">PHP</span>
+      <span @click="setFilter"  class="active   text-black rounded-xl px-3 xl:px-6 py-0.5 mr-4 mb-4 cursor-pointer" id="all">All</span>
+      <span @click="setFilter" class=" bg-white shadow-sm dark:text-gray-700  text-black rounded-xl px-3 xl:px-6 py-0.5 mr-4 mb-4 cursor-pointer" id="react">React</span>
+      <span @click="setFilter" class=" bg-white shadow-sm dark:text-gray-700   text-black  rounded-xl px-3 xl:px-6 py-0.5 mr-4 mb-4 cursor-pointer" id="wordpress">WordPress</span>
+      <span @click="setFilter" class=" bg-white shadow-sm dark:text-gray-700    text-black  rounded-xl px-3 xl:px-6 py-0.5 mr-4 mb-4 cursor-pointer" id="php">PHP</span>
   </div>
 </template>
 
@@ -21,24 +21,26 @@ export default {
     }
   },
   methods: {
-    toggleActive(button){
-      button.classList.toggle('active');
-      button.classList.toggle('inactive');
+    toggleActive(buttonClicked){
+     const childrenElement = this.$refs.parent.children;
+     const children = [...childrenElement];
+     children.forEach( child =>{
+       if(child.classList.contains('active') || child.id === buttonClicked){
+         child.classList.toggle('active');
+       }
+     });
     },
     setFilter(event){
       const buttonClicked = event.target.id;
-<<<<<<< HEAD
-=======
       const isActive = !this.filters[buttonClicked];
->>>>>>> filter
       const updatedFilters = {
         all: false,
         react: false,
         wordpress: false,
         php: false,
-        [buttonClicked]: !this[buttonClicked],
+        [buttonClicked]: isActive,
       }
-<<<<<<< HEAD
+      this.toggleActive(buttonClicked);
       this.$emit('change-filter', updatedFilters);
   
   }
@@ -46,10 +48,6 @@ export default {
   computed:{
     activeAll(){
       return { active: this.filters.all}
-=======
-      this.$emit('change-filter', updatedFilters)
-      
->>>>>>> filter
     }
   }
 }
@@ -58,6 +56,7 @@ export default {
 <style scoped>
   .active{
     background-color: blue;
+    color: white;
   }
 
   /* html.dark .active{
